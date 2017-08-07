da069a0ed1ddbc573234578217fb874bd393c514
Matt Menzenski <matt@mobilgov.com>
2017-08-06 20:56:14 -0500

    update init .py files

---
+++ trunc/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from trunc.__about__ import (
+    __version__, __author__, __author_email__, __maintainer__,
+    __maintainer_email__, __url__, __license__, __description__,
+)
+
+__all__ = [
+    "__version__", "__author__", "__author_email__", "__maintainer__",
+    "__maintainer_email__", "__url__", "__license__", "__description__",
+]
