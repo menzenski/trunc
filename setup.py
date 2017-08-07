8ea83b9688ec3e38fb9386c3c51dc8a91748affb
Matt Menzenski <matt@mobilgov.com>
2017-08-06 20:07:39 -0500

    initial commit for rewrite

--- setup.py
+++ setup.py
@@ -1,15 +1,48 @@
-# -*- coding: utf-8 -*-
+"""
+A Tool for the Russian National Corpus
+"""
+from setuptools import find_packages, setup
 
-from setuptools import setup, find_packages
-
-import trunc
+dependencies = ['click']
 
 setup(
     name='trunc',
-    version=trunc.__version__,
-    url=trunc.__url__,
-    license='MIT',
-    author=trunc.__author__,
-    author_email=trunc.__author_email__,
-    description='Utilities for scraping the Russian National Corpus',
-    )
+    version='0.0.1',
+    url='https://github.com/menzenski/trunc',
+    license='BSD',
+    author='Matt Menzenski',
+    author_email='matt.menzenski@gmail.com',
+    description='A Tool for the Russian National Corpus',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'trunc = trunc.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
