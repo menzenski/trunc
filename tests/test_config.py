df922bb4f2333a740ade069646a58dabb4ab83eb
Matt Menzenski <matt@mobilgov.com>
2017-08-08 21:17:07 -0500

    update test of version number

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,4 +6,4 @@ import trunc
 def test_version_number():
     package_version = trunc.__version__
     print("package version is {}".format(package_version))
-    assert '0.0.1a1' == package_version
+    assert package_version and isinstance(package_version, str)
